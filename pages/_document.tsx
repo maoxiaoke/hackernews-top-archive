d93ef9a3aeba959d7218e97a8802ea45fa6a4717
maoxiaoke <maoxiaoke@outlook.com>
2025-08-16 17:08:30 +0800

    feat: add Umami analytics script to the document head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="f338b849-9069-4474-9dcb-1758999eafca"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
